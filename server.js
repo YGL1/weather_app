bd999f596e7f17f4a2df6833cb1f607c25a576f4
YGL1 <46554130+YGL1@users.noreply.github.com>
2021-08-05 22:14:49 +0300

    configure cors

--- server.js
+++ server.js
@@ -6,9 +6,27 @@ const path = require('path');
 
 const app = express();
 
+const whitelist = ['https://dataservice.accuweather.com/']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+app.use(cors(corsOptions));
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/weather-app'));
-app.use(cors())
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
