c74274426cf49e4ae71cc9a1b5afd6728f4eb0bc
YGL1 <46554130+YGL1@users.noreply.github.com>
2021-08-05 15:55:13 +0300

    install cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //Install express server
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/weather-app'));
+app.use(cors())
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
